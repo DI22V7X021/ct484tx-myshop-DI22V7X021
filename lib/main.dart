eaa6ae28083a3add330ca0ec6db49b41611b42fa
DI22V7X021 <nhdang84st7@gmail.com>
2024-12-08 21:18:28 +0700

    Xay dung trang gio hang

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,41 @@
 import 'package:flutter/material.dart';
-import 'ui/products/products_overview_screen.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+import 'ui/cart/cart_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = ColorScheme.fromSeed(
-      seedColor: Colors.purple,
-      secondary: Colors.deepOrange,
-      surface: Colors.white,
-      surfaceTint: Colors.grey[200],
-    );
-
     final themeData = ThemeData(
-      fontFamily: 'Lato',
-      colorScheme: colorScheme,
-      appBarTheme: AppBarTheme(
-        backgroundColor: colorScheme.primary,
-        foregroundColor: colorScheme.onPrimary,
-        elevation: 4,
-        shadowColor: colorScheme.shadow,
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        primary: Colors.blue,
+        onSurface: Colors.black,
+      ),
+      dialogTheme: DialogTheme(
+        titleTextStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+        contentTextStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+        ),
       ),
     );
 
     return MaterialApp(
-      title: 'MyShop',
+      title: 'My Shop',
       debugShowCheckedModeBanner: false,
       theme: themeData,
       // Hiệu chỉnh trang home
       home: const SafeArea(
-        child: ProductsOverviewScreen(),
+        child: CartScreen(),
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MyApp());
 }
